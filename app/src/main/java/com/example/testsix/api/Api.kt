447a0bf46409902a94032329709fa394b9f26736
shalva96 <gmshalva@gmail.com>
2024-01-09 23:36:20 +0400

    init

---
+++ app/src/main/java/com/example/testsix/api/Api.kt
@@ -0,0 +1,9 @@
+package com.example.testsix.api
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class Api: Application() {
+
+}
---
+++ app/src/main/java/com/example/testsix/presentation/home/HomeFragment.kt
@@ -0,0 +1,23 @@
+package com.example.testsix.presentation.home
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.testsix.R
+import com.example.testsix.databinding.FragmentHomeBinding
+import com.example.testsix.presentation.common.BaseFragment
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class HomeFragment : BaseFragment<FragmentHomeBinding>(FragmentHomeBinding::inflate) {
+    override fun start() {
+
+    }
+
+    override fun clickListener() {
+
+    }
+
+}
